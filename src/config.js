2986af23f0b7ed5807e9781388fbd09bf3a80093
AntonWeel <107237601+AntonWeel@users.noreply.github.com>
2022-08-10 09:24:11 +0500

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,7 @@
+import { createAlchemyWeb3 } from '@alch/alchemy-web3';
+export const NFTCONTRACT = "0x6e29a561bb69B4211c6f0dF44559Cc91313f1e54";
+export const STAKINGCONTRACT = "0xd46bE65986d485fB1999f0FfedB9adCDe9aB6D49";
+export const polygonscanapi = "https://api-testnet.polygonscan.com/api";
+export const moralisapi = "https://deep-index.moralis.io/api/v2/";
+export const nftpng = "https://ipfs.io/ipfs/QmetESUb7wTPr8mBHWFsiSKMw6HTLWUGoL8DrkcaY9TTyh/";
+export const Web3Alc = createAlchemyWeb3("https://polygon-mumbai.g.alchemy.com/v2/unmxnvIfpfdw-wKtl6g80-Bk5ODvdf7p");
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import reportWebVitals from './reportWebVitals';
+import { Routes, Route } from "react-router-dom";
+import Nft from './nft';
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
+<Routes>
+<Route path="/nft" element={<Nft />} />
+</Routes>
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StrictMode>
+  
+  <React.StrictMode>
     <App />
-  </StrictMode>
+    <Nft />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
